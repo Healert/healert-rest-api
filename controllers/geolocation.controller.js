2f1b64ac397837d8d8364cd0ba41160444f9ca1d
Rodgers Kipchirchir Tanui <rtanui@safaricom.co.ke>
2020-04-12 11:51:12 +0300

    Added validation to geolocation endpoint

--- controllers/geolocation.controller.js
+++ controllers/geolocation.controller.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const Joi = require('@hapi/joi');
 const Geolocation = require('../models/geolocation.model');
 
 const getlocationController = {
@@ -18,7 +19,41 @@ const getlocationController = {
 		} catch (error) {
 			return error;
 		}
-	}
+	},
+
+	/**
+	 * Joi validation schema for longitude and latitude
+	 */
+	saveValidationSchema: Joi.object({
+		longitude: Joi.number().required().error((errors) => {
+			return errors.map(error => {
+				switch (error.type) {
+					case "number.base":
+						return {
+							message: "Longitude must be number"
+						};
+					case "any.required":
+						return {
+							message: "Longitude is required"
+						};
+				}
+			})
+		}),
+		latitude: Joi.number().required().error((errors) => {
+			return errors.map(error => {
+				switch (error.type) {
+					case "number.base":
+						return {
+							message: "Latitude must be number"
+						};
+					case "any.required":
+						return {
+							message: "Latitude is required"
+						};
+				}
+			})
+		})
+	})
 }
 
 module.exports = getlocationController;
--- routes/geolocation.route.js
+++ routes/geolocation.route.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const Boom = require('@hapi/boom');
+
 const getlocationController = require('../controllers/geolocation.controller');
 
 module.exports = [{
@@ -10,17 +11,30 @@ module.exports = [{
 	},
 	path: '/api/geo-locations',
 	handler: async (request, h) => {
+
 		try {
-			const payload = {
-				user_id: request.user.user_id,
-				coordinates: [request.payload.longitude, request.payload.latitude],
-				timestamp: request.payload.timestamp
-			};
-			if (!request.payload.longitude) {
-				return Boom.badRequest('Longitude is required');
-			} else if (!request.payload.latitude) {
-				return Boom.badRequest('Latitude is required');
+			const coordinates = {
+				longitude: request.payload.longitude,
+				latitude: request.payload.latitude
+			}
+			const {
+				error
+			} = getlocationController.saveValidationSchema.validate(coordinates, {
+				abortEarly: false
+			});
+			if (error) {
+				const err = Boom.badRequest('Missing required fields', {
+					error: error.details
+				});
+				err.output.payload.details = err.data;
+				return err;
 			} else {
+				const payload = {
+					user_id: request.user.user_id,
+					coordinates: [request.payload.longitude, request.payload.latitude],
+					timestamp: request.payload.timestamp
+				};
+
 				const output = await getlocationController.saveLocation(payload);
 				const response = {
 					type: 'Success',
@@ -31,7 +45,7 @@ module.exports = [{
 			}
 
 		} catch (error) {
-			return Boom.badImplementation();
+			return Boom.badImplementation(error);
 		}
 	}
 }];
