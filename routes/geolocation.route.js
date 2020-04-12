f4a446d293b8e0cb39bff8507a560d38ce204678
Rodgers Kipchirchir Tanui <rtanui@safaricom.co.ke>
2020-04-12 17:15:56 +0300

    Added functionality to save bulk geolocations

--- routes/geolocation.route.js
+++ routes/geolocation.route.js
@@ -43,5 +43,48 @@ module.exports = [{
 		} catch (error) {
 			return Boom.badImplementation();
 		}
+	},
+	method: 'POST',
+	config: {
+		auth: 'firebase',
+		description: 'Upload Bulk Location Logs',
+		notes: 'Returns the newly created bulk location logs',
+		tags: ['api'],
+		validate: {
+			payload: Joi.object({
+				locations: Joi.array().items({
+					longitude: Joi.number().min(-90).max(90)
+						.required().description('Longitude'),
+					latitude: Joi.number().min(-180).max(180)
+						.required().description('Latitude'),
+					timestamp: Joi.date().required().timestamp('unix')
+						.description('Log unix timestamp'),
+				}),
+			}),
+
+			failAction: async (request, h, err) => {
+				throw err;
+			}
+		}
+	},
+	path: '/api/geo-locations/bulk',
+	handler: async (request, h) => {
+		try {
+			const payload = request.payload.locations;
+			const user_id = request.user.user_id;
+
+			const output = await getlocationController.uploadBulk(payload, user_id);
+
+			const response = {
+				type: 'Success',
+				message: 'Geolocations saved successfully ',
+				body: output
+			};
+
+			return h.response(response);
+
+		} catch (error) {
+			return Boom.badImplementation();
+		}
 	}
 }];
