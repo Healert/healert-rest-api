2d28c5f3d68638e578c333861c3087d92c53e712
Emmanuel Nyachoke <achachiez@gmail.com>
2020-04-13 18:00:38 +0300

    Add CD

---
+++ scripts/deploy.sh
@@ -0,0 +1,16 @@
+docker login  -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD" 
+export BUILD="build-$(git rev-parse --short ${TRAVIS_COMMIT})"
+docker build -t enyachoke/healert-rest-api:$BUILD .
+export API_IMAGE=enyachoke/healert-rest-api:$BUILD
+docker push $CONFIG_IMAGE
+export PATH=$PATH:$PWD/
+git clone https://$GITHUB_USERNAME:$GITHUB_PASSWORD@github.com/healert-rest-api-cd-config
+export APP_NAME=$TRAVIS_BRANCH
+export APP_NAME_LOWER=$(echo $APP_NAME | tr "[:upper:]" "[:lower:]")
+cd healert-rest-api-cd-config/
+git checkout $APP_NAME || git checkout -b $APP_NAME
+git pull origin master -s recursive -X theirs
+sed -i "s~.*image=.*~  image='$CONFIG_IMAGE',~g" healert-jsonnet/healert-jsonnet.jsonnet
+git add .
+git commit -m "Update image to $CONFIG_IMAGE"
+git push origin $APP_NAME -f
