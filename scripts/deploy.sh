9c98af0a5314317e6f4d7623ddf4f2111c319f4c
Emmanuel Nyachoke <achachiez@gmail.com>
2020-04-13 18:19:06 +0300

    Adding CD

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,10 +7,9 @@ export PATH=$PATH:$PWD/
 git clone https://$GITHUB_USERNAME:$GITHUB_PASSWORD@github.com/Healert/healert-rest-api-cd-config.git
 export APP_NAME=$TRAVIS_BRANCH
 export APP_NAME_LOWER=$(echo $APP_NAME | tr "[:upper:]" "[:lower:]")
-cd healert-rest-api-cd-config/
-git checkout $APP_NAME || git checkout -b $APP_NAME
+cd healert-rest-api-cd-config
 git pull origin master -s recursive -X theirs
 sed -i "s~.*image=.*~  image='$CONFIG_IMAGE',~g" healert-jsonnet/healert-jsonnet.jsonnet
-git add .
+git add -A
 git commit -m "Update image to $CONFIG_IMAGE"
-git push origin $APP_NAME -f
+git push origin master -f
